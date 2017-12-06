8d6d3f9b9f3404386a4ed52525d48628fba1b144
zaki <zachariasz.strus@gmail.com>
2017-12-06 12:38:43 +0100

    fix

--- src/app/server-connection/order.ts
+++ src/app/server-connection/order.ts
@@ -1,13 +1,14 @@
 export interface Order{
-    id:number,
-    tableNumber:number,
-    items:Item[],
-    status:Status
-  }
+  id:number,
+  tableNumber:number,
+  items:Item[],
+  status:Status
+}
 
 export interface Item{
-    name:string,
-    price:number
-  }
+  name:string,
+  price:number,
+  quantity: number
+}
 
-export enum Status { DONE, PENDING, CANCELLED };
+export enum Status { DONE, PENDING, CANCELLED }
